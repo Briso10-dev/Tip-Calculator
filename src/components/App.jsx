c99d585c0df38f4d83b18152c98fde9a0acea6df
Briso10-dev <kouambrice10@gmail.com>
2025-07-02 20:33:13 +0200

    Lifting up the BIll useState variable to App-parent

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Bill from "./Bill"
 import Service from "./Service"
 import Conclusion from "./Conclusion"
@@ -5,12 +6,18 @@ import Reset from "./Reset"
 
 function App() {
 
+  const [bill,setBill] = useState("");
+  function handleBill(e){
+      setBill(Number(e.target.value));
+  }
+
+
   return (
     <div className="flex flex-col justify-center gap-6 bg-gray-50">
-      <Bill />
+      <Bill bill={bill} onSetBill={handleBill} />
       <Service text="How did you like the service" />
       <Service text="How did your friend like the service"/>
-      <Conclusion />
+      <Conclusion/>
       <Reset />
     </div>
   )
--- src/components/Bill.jsx
+++ src/components/Bill.jsx
@@ -1,8 +1,5 @@
-import { useState } from "react";
 
-function Bill(){
-    const [bill,setBill] = useState("");
-
+function Bill({bill,onSetBill}){
 
     return(
         <div className="flex items-center gap-6 text-4xl font-medium">
@@ -11,7 +8,7 @@ function Bill(){
             type="text"
             className="outline-none border-s-8 rounded-md border-gray-200 p-2 text-2xl bg-gray-200"
             value={bill} 
-            onChange={(e) => setBill(Number(e.target.value))}
+            onChange={onSetBill}
             />
         </div>
     )
