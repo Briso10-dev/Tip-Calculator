ac2ec12aa6d1b0ba0d6e93d14c5759dcdc0726f1
Briso10-dev <kouambrice10@gmail.com>
2025-07-02 20:13:59 +0200

    Rendering the BIll component a control element

--- src/components/Bill.jsx
+++ src/components/Bill.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 
 function Bill(){
-    const [bill,setBill] = useState(1);
+    const [bill,setBill] = useState("");
+
 
     return(
         <div className="flex items-center gap-6 text-4xl font-medium">
@@ -9,7 +10,9 @@ function Bill(){
             <input 
             type="text"
             className="outline-none border-s-8 rounded-md border-gray-200 p-2 text-2xl bg-gray-200"
-            value={bill} />
+            value={bill} 
+            onChange={(e) => setBill(Number(e.target.value))}
+            />
         </div>
     )
 
