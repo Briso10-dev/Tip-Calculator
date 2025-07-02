37f6abeba94fc56ab6cc29bf9c51df544ee75967
Briso10-dev <kouambrice10@gmail.com>
2025-07-02 19:57:16 +0200

    All possibly styling done with as for now

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,13 +6,13 @@ import Reset from "./Reset"
 function App() {
 
   return (
-    <>
+    <div className="flex flex-col justify-center gap-6 bg-gray-50">
       <Bill />
       <Service text="How did you like the service" />
       <Service text="How did your friend like the service"/>
       <Conclusion />
       <Reset />
-    </>
+    </div>
   )
 }
 
