1077944b6aa30dd74fd133d0ff3fb094ed8223bf
Briso10-dev <kouambrice10@gmail.com>
2025-07-02 19:15:38 +0200

    BIll component prototype

--- src/components/Bill.jsx
+++ src/components/Bill.jsx
@@ -1,9 +1,11 @@
 
 function Bill(){
     return(
-        <div className="flex items-center gap-2">
-            <p>How much was the bill ? </p>
-            <input type="text" />
+        <div className="flex items-center gap-4 text-4xl font-medium">
+            <p className="m-10">How much was the bill ? </p>
+            <input 
+            type="text"
+            className="outline-none border-s-8 rounded-md border-gray-200 p-2 text-2xl bg-gray-200" />
         </div>
     )
 
