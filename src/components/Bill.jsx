b3b38bd15aa9a7e1e612d3e20ae4a79f623c8e11
Briso10-dev <kouambrice10@gmail.com>
2025-07-02 19:21:06 +0200

    Service component creation and other style fixed

--- src/components/Bill.jsx
+++ src/components/Bill.jsx
@@ -2,7 +2,7 @@
 function Bill(){
     return(
         <div className="flex items-center gap-4 text-4xl font-medium">
-            <p className="m-10">How much was the bill ? </p>
+            <p>How much was the bill ? </p>
             <input 
             type="text"
             className="outline-none border-s-8 rounded-md border-gray-200 p-2 text-2xl bg-gray-200" />
---
+++ src/components/Service.jsx
@@ -0,0 +1,19 @@
+
+function Service(){
+     return(
+        <div className="flex items-center gap-4 text-4xl font-medium">
+            <p> How did you like the service? </p>
+            <select 
+            type="text"
+            className="outline-none border-s-8 rounded-md border-gray-200 p-2 text-2xl bg-gray-200">
+            <option value="Dissatisfied">Dissatisfied(0%)</option>
+            <option value="okay">It was okay(5%)</option>
+            <option value="good">It was good(10%)</option>
+            <option value="wow">Absolute amazing!</option>      
+            </select>
+        </div>
+    )
+
+}
+
+export default Service;
