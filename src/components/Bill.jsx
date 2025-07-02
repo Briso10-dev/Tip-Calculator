d18b21dca184576b3e78dbc939e146bfdf4d2ff6
Briso10-dev <kouambrice10@gmail.com>
2025-07-02 20:03:02 +0200

    Definding a useState variable tp BIll component

--- src/components/Bill.jsx
+++ src/components/Bill.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 
 function Bill(){
+    const [bill,setBill] = useState(1);
+
     return(
         <div className="flex items-center gap-6 text-4xl font-medium">
             <p>How much was the bill ? </p>
             <input 
             type="text"
-            className="outline-none border-s-8 rounded-md border-gray-200 p-2 text-2xl bg-gray-200" />
+            className="outline-none border-s-8 rounded-md border-gray-200 p-2 text-2xl bg-gray-200"
+            value={bill} />
         </div>
     )
 
