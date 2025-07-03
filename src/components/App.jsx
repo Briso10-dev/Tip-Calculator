6c938cc0ac6e299934135181cc8fabe2d018ac0c
Briso10-dev <kouambrice10@gmail.com>
2025-07-03 19:02:44 +0200

    Creating 2 useState for the same component Service

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,8 @@ import Reset from "./Reset"
 function App() {
 
   const [bill, setBill] = useState("");
-  const [service, setService] = useState("");
+  const [percentage1, setPercentage1] = useState(0);
+  const [percentage2, setPercentage2] = useState(0);
 
   function handleBill(e) {
     setBill(Number(e.target.value));
@@ -16,8 +17,13 @@ function App() {
   return (
     <div className="flex flex-col justify-center gap-6 bg-gray-50">
       <Bill bill={bill} onSetBill={handleBill} />
-      <Service>How did you like the service</Service>
-      <Service>How did your friend like the service
+      <Service 
+      
+    
+        How did you like the service</Service>
+      <Service 
+      
+      How did your friend like the service
       </Service>
       <Conclusion bill={bill} />
       <Reset />
