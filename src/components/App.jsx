b08b99e0889f9f544126fd94e7d481a3938b67ba
Briso10-dev <kouambrice10@gmail.com>
2025-07-03 18:57:45 +0200

    continue changing with conclusion component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ import Reset from "./Reset"
 function App() {
 
   const [bill, setBill] = useState("");
-  const [service,setService] = useState("");
+  const [service, setService] = useState("");
 
   function handleBill(e) {
     setBill(Number(e.target.value));
@@ -17,7 +17,8 @@ function App() {
     <div className="flex flex-col justify-center gap-6 bg-gray-50">
       <Bill bill={bill} onSetBill={handleBill} />
       <Service>How did you like the service</Service>
-      <Service>How did your friend like the service</Service>
+      <Service>How did your friend like the service
+      </Service>
       <Conclusion bill={bill} />
       <Reset />
     </div>
--- src/components/Bill.jsx
+++ src/components/Bill.jsx
@@ -6,6 +6,7 @@ function Bill({bill,onSetBill}){
             <label>How much was the bill ? </label>
             <input 
             type="text"
+            placeholder="Bill value"
             className="outline-none border-s-8 rounded-md border-gray-200 p-2 text-2xl bg-gray-200"
             value={bill} 
             onChange={onSetBill}
