e137a734b0a7bbec4764c1a61d3b6409f734d40f
Briso10-dev <kouambrice10@gmail.com>
2025-07-03 19:40:40 +0200

    Correcting minor bugs woth the bill> 0 formating

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,36 +9,37 @@ function App() {
   const [bill, setBill] = useState("");
   const [percentage1, setPercentage1] = useState(0);
   const [percentage2, setPercentage2] = useState(0);
-  
+
   function handleBill(e) {
     setBill(Number(e.target.value));
   }
-  function handleReset(){
+  function handleReset() {
     setBill("");
     setPercentage1(0);
     SetPercentage2(0);
   }
 
-  const tip = (bill * (percentage1 + percentage2) /2 /100);  //Here a derived State
+  const tip = (bill * (percentage1 + percentage2) / 2 / 100);  //Here a derived State
 
   return (
     <div className="flex flex-col justify-center gap-6 bg-gray-50">
       <Bill bill={bill} onSetBill={handleBill} />
-      <Service 
-       percentage = {percentage1}
-       onSelect = {setPercentage1}>
+      <Service
+        percentage={percentage1}
+        onSelect={setPercentage1}>
         How did you like the service
       </Service>
-      <Service 
-      percentage = {percentage2}
-      onSelect = {setPercentage2}>
-      How did your friend like the service
+      <Service
+        percentage={percentage2}
+        onSelect={setPercentage2}>
+        How did your friend like the service
       </Service>
-     {bill > 0 && (
-      <>
-       <Conclusion bill={bill} tip= {tip} />}
-      <Reset onReset = {handleReset} />
-      </>
+      {bill > 0 && (
+        <>
+          <Conclusion bill={bill} tip={tip} />
+          <Reset onReset={handleReset} />
+        </>
+      )}
     </div>
   )
 }
